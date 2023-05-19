dc49f91ec3624a7fc229125cf76ed7700c902ccb
Ryan Marganti <rmarganti@gannett.com>
2023-05-19 09:44:38 -0400

    scrtsync brew formula template

---
+++ scrtsync.rb
@@ -0,0 +1,11 @@
+class <Projectname capitalized> < Formula
+  desc "Sync secrets between Vault, K8s, and .env files"
+  homepage "https://github.com/rmarganti/scrtsync"
+  url "-----"
+  version "-----"
+  sha256 "-----"
+
+  def install
+    bin.install "<bin-name>"
+  end
+end
