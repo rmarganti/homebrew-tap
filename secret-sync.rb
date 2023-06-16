518cd5158ed9b145307cce6fce5f8a7b4f80db7e
Ryan Marganti <ryan.marganti@gmail.com>
2023-06-16 14:23:42 -0400

    updating formula for secret-sync

--- secret-sync.rb
+++ secret-sync.rb
@@ -1,9 +1,9 @@
 class <Projectname capitalized> < Formula
   desc "Sync secrets between Vault, K8s, and .env files"
   homepage "https://github.com/rmarganti/scrtsync"
-  url "-----"
-  version "-----"
-  sha256 "-----"
+  url "https://github.com/rmarganti/scrtsync/releases/download/v1.1.0/secret-sync-v1.1.0-x86_64-macos.tar.xz"
+  version "1.1.0"
+  sha256 "6ffeefa031c5c277df603cf9312750e808fd8ca36a0e26cff35fe3c239ef3e79"
 
   def install
     bin.install "<bin-name>"
